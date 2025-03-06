5d7a0d3df28645cb07e7e1b1bc682b1c8c78e6b0
golap-raj <golapraj47@gmail.com>
2025-03-07 05:53:38 +0600

    resolve

---
+++ data.js
@@ -0,0 +1,17 @@
+
+
+// let name = "Golap";
+// console.log(name);
+
+// let newName = name;
+
+// console.log(newName)
+
+// name = "Tahmid";
+// console.log(newName)
+
+let person = {Name : "Golap"};
+
+let NewName = person;
+person.Name = "tahmid   "
+console.log(NewName)
